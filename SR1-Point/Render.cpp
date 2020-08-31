4b7806df03502da6ccc0ac48f87d12b1f02705e8
Amado-Developer <amado_developer@hotmail.com>
2020-08-30 18:22:31 -0600

    Creating project

---
+++ SR1-Point/Render.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Amado Garcia on 8/30/20.
+//
+
+#include "Render.h"
---
+++ SR1-Point/Render.h
@@ -0,0 +1,15 @@
+//
+// Created by Amado Garcia on 8/30/20.
+//
+
+#ifndef SR1_POINT_RENDER_H
+#define SR1_POINT_RENDER_H
+
+class Render {
+    Render();
+
+
+};
+
+
+#endif //SR1_POINT_RENDER_H
