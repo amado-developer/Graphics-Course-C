bf94868583d04b065e990cdebcd9afc5fca4417b
Amado-Developer <amado_developer@hotmail.com>
2020-09-16 16:50:50 -0600

    just some bugs to fix

--- Transformations/main.cpp
+++ Transformations/main.cpp
@@ -11,19 +11,19 @@ int main()
     Texture t("model.bmp");
     t.read();
     Render r(t);
-    r.glCreateWindow(1920, 1080);
-    r.glViewPort(0, 0, 1920, 1080);
+    r.glCreateWindow(800, 600);
+    r.glViewPort(0, 0, 800, 600);
     r.glClearColor(0.1, 0.1, 0.1);
     r.glClear(false);
     r.setIsPixels(true);
     tuple<double, double, double> modelPosition(0.0,0.0,-5.0);
     r.createViewMatrix(make_tuple(0.0,0.0,0.0), make_tuple(0.0,0.0,0.0));
     r.createProjectionMatrix(0.1,1000.0,60.0);
-//    r.lookAt(modelPosition, make_tuple(2.0, 2.0, 0.0));
+    r.lookAt(modelPosition, make_tuple(2.0, 2.0, 0.0));
     r.setLight(math.norm(make_tuple(0.0, 0.0, 1.0)));
     Shaders s;
     r.setActiveRender(&s.gourad);
-    r.load("model.obj", {0.0, 0.0, -5}, {4.0, 4.0, 4.0}, {0.0,0.0,0.0});
+    r.load("model.obj", {0.0, 0.0, -5}, {2.0, 2.0, 2.0}, {0.0,0.0,0.0});
     r.glFinish("out.bmp");
     return 0;
 }
